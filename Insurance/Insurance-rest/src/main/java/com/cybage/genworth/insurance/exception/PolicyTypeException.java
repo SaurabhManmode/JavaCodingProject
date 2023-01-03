47e84bb73c2bf9e9d56b1dab81d5e937ff7027bc
Saurabh Manmode <saurabh.manmode94@gmail.com>
2023-01-03 15:18:13 +0530

    main branch

--- Insurance/Insurance-rest/src/main/java/com/cybage/genworth/insurance/exception/PolicyTypeException.java
+++ Insurance/Insurance-rest/src/main/java/com/cybage/genworth/insurance/exception/PolicyTypeException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)  //id is incorrect
 public class PolicyTypeException extends RuntimeException {
 	
+	private String name;
+	
 	private static final long serialVersionUID = 1L;
 
 	public PolicyTypeException(String message){
